acc8840a8408ae5a8cf7fcc02b613e1e0f30e271
zLeoAlex <z.alex.leo@outlook.com>
2021-05-15 17:16:05 +0800

    fix: example judger null-line fix

--- examples/A+B Problem/judger/src/main.rs
+++ examples/A+B Problem/judger/src/main.rs
@@ -1,18 +1,12 @@
-use std::{
-    env::var,
-    io::{stdin, BufRead},
-};
+use std::{env::var, process::exit};
 
-use a_plus_b_judger::pb::{self, JudgerRequest, TestCode, TestResult};
+use a_plus_b_judger::pb::{self, judger_client::JudgerClient, JudgerRequest, TestCode, TestResult};
+use tokio::io::{AsyncBufReadExt, BufReader};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let connect_addr = var("JUDGER_ADDR")?;
-    let name = var("JOB_ID")?;
-    let apikey = var("APIKEY")?;
-
-    let mut client =
-        pb::judger_client::JudgerClient::connect(format!("grpc://{}", connect_addr)).await?;
+    let mut client = ResultPoster::default().await;
+    let mut buf_stdin = BufReader::new(tokio::io::stdin());
 
     for _ in 0..5000 {
         let a = rand::random::<u32>() >> 1;
@@ -20,22 +14,90 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{} {}", a, b);
 
         let mut line = String::new();
-        stdin().lock().read_line(&mut line)?;
-        let ans: u32 = line.parse()?;
-        if ans != a + b {
-            client
-                .post_test_result(JudgerRequest {
-                    job_id: name.clone(),
-                    api_key: apikey.clone(),
-                    result: TestResult {
-                        other_msg: Some(format!("{} + {} = {}, but output {}.", a, b, a + b, ans)),
-                        code: TestCode::WrongAnswer.into(),
-                    },
-                })
-                .await?;
-            return Ok(());
+        while line.trim() == "" {
+            buf_stdin.read_line(&mut line).await?;
+        }
+
+        match line.trim().parse::<u32>() {
+            Ok(ans) => {
+                if ans != a + b {
+                    if let Some(client) = &mut client {
+                        client
+                            .post(
+                                TestCode::WrongAnswer,
+                                Some(format!("{} + {} = {}, but output {}.", a, b, a + b, ans)),
+                            )
+                            .await;
+                    } else {
+                        eprintln!("Wrong answer");
+                    }
+                    exit(1);
+                }
+            }
+            Err(e) => {
+                if let Some(client) = &mut client {
+                    client
+                        .post(
+                            TestCode::Other,
+                            Some(format!("Line \"{}\"Presentation error: {}", line, e)),
+                        )
+                        .await;
+                } else {
+                    eprintln!("Cannot scan input \"{}\": {}", line, e);
+                    exit(1);
+                }
+            }
         }
     }
 
+    if let Some(mut client) = client {
+        client.post(TestCode::Accepted, None).await;
+    }
     Ok(())
+}
+
+struct ResultPoster {
+    client: JudgerClient<tonic::transport::channel::Channel>,
+    name: String,
+    apikey: String,
+}
+
+impl ResultPoster {
+    pub async fn default() -> Option<Self> {
+        if let (Ok(connect_addr), Ok(name), Ok(apikey)) =
+            (var("JUDGER_ADDR"), var("JOB_ID"), var("APIKEY"))
+        {
+            if let Some(client) =
+                pb::judger_client::JudgerClient::connect(format!("grpc://{}", connect_addr))
+                    .await
+                    .ok()
+            {
+                Some(ResultPoster {
+                    client,
+                    name,
+                    apikey,
+                })
+            } else {
+                eprintln!("Connect error to {}", connect_addr);
+                None
+            }
+        } else {
+            eprintln!("No ENV_VAR, may running in bare environment");
+            None
+        }
+    }
+
+    pub async fn post(&mut self, code: TestCode, other_msg: Option<String>) {
+        let _ = self
+            .client
+            .post_test_result(JudgerRequest {
+                job_id: self.name.clone(),
+                api_key: self.apikey.clone(),
+                result: TestResult {
+                    other_msg,
+                    code: code.into(),
+                },
+            })
+            .await;
+    }
 }
